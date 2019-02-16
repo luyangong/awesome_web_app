33fd21eea73a9c905c81acee970b5c3e515ad413
luyangong <luyangong@qq.com>
2019-02-16 23:29:35 +0800

    test for changing app

--- myapp/app.py
+++ myapp/app.py
@@ -1,8 +1,13 @@
 from aiohttp import web
 
-async def hello(request):
-    return web.Response(text="Hello, world")
-
-app = web.Application()
-app.add_routes([web.get('/', hello)])
-web.run_app(app, host='127.0.0.1')
+# async def hello(request):
+#     return web.Response(text="Hello, world")
+#
+# app = web.Application()
+# app.add_routes([web.get('/', hello)])
+# web.run_app(app, host='127.0.0.1')
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.getcwd()), 'www'))
+from www.app import myapp
+web.run_app(myapp(), host='127.0.0.1')
