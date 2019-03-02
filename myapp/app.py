0c9a5b39b01a8c803a01be6883324dee6dad80a7
luyangong <luyangong@qq.com>
2019-03-02 23:02:57 +0800

    test for using websocket

--- myapp/app.py
+++ myapp/app.py
@@ -1,13 +1,32 @@
 from aiohttp import web
+import aiohttp
 
 # async def hello(request):
 #     return web.Response(text="Hello, world")
 #
-# app = web.Application()
+app = web.Application()
 # app.add_routes([web.get('/', hello)])
 # web.run_app(app, host='127.0.0.1')
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(os.getcwd()), 'www'))
-from www.app import myapp
-web.run_app(myapp(), host='127.0.0.1')
+async def websocket_handler(request):
+
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                print('gooooooooooooooooooooooooooooooood!')
+                await ws.send_str(msg.data + '/answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print('ws connection closed with exception %s' %
+                  ws.exception())
+
+    print('websocket connection closed')
+
+    return ws
+app.add_routes([web.get('/ws', websocket_handler)])
+web.run_app(app, host='127.0.0.1')
